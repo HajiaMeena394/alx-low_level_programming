ad0ee18ca1d2cb22de11cc81acc67c3b50a96f15
Meena <harunamina71@gmail.com>
2023-10-15 10:33:27 -0700

    review comb 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,9 +3,8 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Succes)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int tho;
@@ -13,31 +12,19 @@ int main(void)
 	int ten;
 	int bas;
 
-for (tho = 0 ; tho < 10 ; tho++)
-{
-
-	for (hun = 0 ; hun <= 10 ; hun++)
+	for (tho = 0; tho < 10; tho++)
 	{
-
-		for (ten = 0 ; ten < 10 ; ten++)
+		for (hun = 0; hun < 10; hun++)
 		{
-			for (bas = 0 ; bas < 10 ; bas++)
+			for (ten = 0; ten < 10; ten++)
 			{
-				putchar('0' + tho);
-				putchar('0' + hun);
-				putchar(32);
-				putchar('0' + ten);
-				putchar('0' + bas);
-				if (!(tho == 9 && hun == 8))
+				for (bas = 0; bas < 10; bas++)
 				{
-					putchar(',');
-					putchar(32);
+					printf("%02d %02d, ", tho * 100 + hun * 10 + ten, ten * 10 + bas);
 				}
-				bas++;
 			}
 		}
 	}
-}
 	putchar('\n');
 	return (0);
 }
